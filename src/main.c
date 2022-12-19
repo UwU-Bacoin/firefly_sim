b6147053cd1ec0aa9b0651c8f072adc9bdce9598
sigmanificient <edhyjox@gmail.com>
2022-12-19 12:07:05 +0100

    :sparkles: Use firefly increment & display in main

--- src/main.c
+++ src/main.c
@@ -1,17 +1,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "firefly.h"
 
 int main(void)
 {
-    int energy = rand() % 100;
+    srand48(time(NULL) + getpid());
+    double energy = drand48() * 100;
+    double delta_energy = drand48();
 
-    firefly_display(energy, true);
-    energy = 50;
-    firefly_display(energy, true);
-    energy = 150;
-    firefly_display(energy, true);
-    energy = 250;
-    firefly_display(energy, true);
+    for (int i = 0; i < 50; i++) {
+        energy = firefly_increment(energy, delta_energy);
+        firefly_display(energy, true);
+    }
 }
